f9bea22bd7ae2c15f076572ccfeacaf57faa3e62
Tomucha <tomucha@gmail.com>
2020-04-20 17:42:57 +0200

    Items!

---
+++ lib/src/docs/fnx_ui_docs_0080_border.dart
@@ -0,0 +1,13 @@
+import 'package:angular/angular.dart';
+import 'package:angular_forms/angular_forms.dart';
+
+import '../../fnx_ui.dart';
+
+@Component(
+  selector: 'fnx-ui-docs-0080-border',
+  templateUrl: 'fnx_ui_docs_0080_border.html',
+  directives: [fnxUiAllDirectives, coreDirectives, formDirectives],
+)
+class FnxUiDocs0080Border {}
+  
+  
---
+++ lib/src/docs/fnx_ui_docs_0100_item.dart
@@ -0,0 +1,13 @@
+import 'package:angular/angular.dart';
+import 'package:angular_forms/angular_forms.dart';
+
+import '../../fnx_ui.dart';
+
+@Component(
+  selector: 'fnx-ui-docs-0100-item',
+  templateUrl: 'fnx_ui_docs_0100_item.html',
+  directives: [fnxUiAllDirectives, coreDirectives, formDirectives],
+)
+class FnxUiDocs0100Item {}
+  
+  
