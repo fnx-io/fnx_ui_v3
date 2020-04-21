b742fc742c3bef07d2425cf2db9fa4943804126a
Tomucha <tomucha@gmail.com>
2020-04-21 22:53:12 +0200

    Colors demo

---
+++ lib/src/docs/fnx_ui_docs_0090_colors.dart
@@ -0,0 +1,37 @@
+import 'package:angular/angular.dart';
+import 'package:angular_forms/angular_forms.dart';
+
+import '../../fnx_ui.dart';
+
+@Component(
+  selector: 'fnx-ui-docs-0090-colors',
+  templateUrl: 'fnx_ui_docs_0090_colors.html',
+  directives: [fnxUiAllDirectives, coreDirectives, formDirectives],
+)
+class FnxUiDocs0090Colors {
+  List<String> colors = [
+    "black",
+    "white",
+    "grey",
+    "blue-grey",
+    "fnx",
+    "red",
+    "pink",
+    "purple",
+    "deep-purple",
+    "indigo",
+    "blue",
+    "light-blue",
+    "cyan",
+    "teal",
+    "green",
+    "light-green",
+    "lime",
+    "yellow",
+    "amber",
+    "orange",
+    "deep-orange",
+    "brown",
+  ];
+  List<String> flavours = ["", ...List.generate(4, (i) => "--l${i + 1}"), ...List.generate(4, (i) => "--d${i + 1}")];
+}
